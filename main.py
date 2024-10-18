e1dfe182f35481369523af3ba0bb6149f01ba86a
gydro228 <128089215+gydro228@users.noreply.github.com>
2024-10-18 20:31:16 +0300

    Add files via upload
    Практическое задание по теме "Переменные"

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_of_course = 'Python'
+one_task = number_of_hours / number_of_homework
+print(f'Курс: {name_of_course}, всего задач: {number_of_homework}, затрачено часов: {number_of_hours}, среднее время выполнения {one_task} часа.')
